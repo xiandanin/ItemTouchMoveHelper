d7b033b0a89562fa438eb2d2ff30a9b521c4ad50
dengyuhan <464958567@qq.com>
2018-04-08 14:20:20 +0800

    Initial commit

---
+++ app/src/main/java/com/dyhdyh/helper/itemtouch/example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dyhdyh.helper.itemtouch.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
